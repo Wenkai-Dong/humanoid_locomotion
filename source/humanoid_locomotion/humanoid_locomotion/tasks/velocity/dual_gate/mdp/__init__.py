89948e62a12f5a0364a84d4809bc3659d959d4f2
Wenkai Dong <1398673693@qq.com>
2026-04-17 23:54:22 +0800

    add recorders in __init__.py

--- source/humanoid_locomotion/humanoid_locomotion/tasks/velocity/dual_gate/mdp/__init__.py
+++ source/humanoid_locomotion/humanoid_locomotion/tasks/velocity/dual_gate/mdp/__init__.py
@@ -5,3 +5,5 @@ from .observations import *  # noqa: F401
 from .rewards import *  # noqa: F401
 from .curriculums import *  # noqa: F401
 from .terminations import * # noqa: F401
+
+from .recorders import *    # noqa:F401
