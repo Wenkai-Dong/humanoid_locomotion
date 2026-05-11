4baf5a5cd528fab3091db127b797a510aa3bd4fc
Wenkai Dong <1398673693@qq.com>
2026-05-11 16:59:45 +0800

    Refactor CSV generation: replace `csv.py` with `gen_csv.py` for improved structure and data extraction; register new G1 environments with updated Attention and Velocity configurations.

--- source/humanoid_locomotion/humanoid_locomotion/tasks/velocity/dual_gate/config/g1/__init__.py
+++ source/humanoid_locomotion/humanoid_locomotion/tasks/velocity/dual_gate/config/g1/__init__.py
@@ -11,6 +11,7 @@ from . import agents
 # Register Gym environments.
 ##
 
+# DualGate-Flat-G1-v0
 gym.register(
     id="DualGate-Flat-G1-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
@@ -41,6 +42,8 @@ gym.register(
         "rsl_rl_with_symmetry_cfg_entry_point": f"{agents.__name__}.mlp_rsl_rl_ppo_cfg:G1FlatPPORunnerCfgWithSymmetryCfg",
     },
 )
+
+# DualGate-Rough-G1-v0
 gym.register(
     id="DualGate-Rough-G1-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
@@ -71,6 +74,8 @@ gym.register(
         "rsl_rl_with_symmetry_cfg_entry_point": f"{agents.__name__}.cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
     },
 )
+
+# DualGate-Velocity-Rough-G1-v0
 gym.register(
     id="DualGate-Velocity-Rough-G1-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
@@ -101,6 +106,8 @@ gym.register(
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
+
+# DualGate-Attention-G1-v0
 gym.register(
     id="DualGate-Attention-G1-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
@@ -109,9 +116,6 @@ gym.register(
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
 gym.register(
@@ -122,9 +126,6 @@ gym.register(
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfg_PLAY",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
 gym.register(
@@ -135,11 +136,10 @@ gym.register(
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfg_EVAL",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
+
+# DualGate-Attention-G1-v1
 gym.register(
     id="DualGate-Attention-G1-v1",  # change terrain curriculum, add height on curriculum
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
@@ -148,9 +148,6 @@ gym.register(
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfgv1",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
 gym.register(
@@ -161,9 +158,6 @@ gym.register(
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfgv1_PLAY",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
 gym.register(
@@ -174,11 +168,10 @@ gym.register(
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfgv1_EVAL",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
+
+# DualGate-Attention-G1-v2
 gym.register(
     id="DualGate-Attention-G1-v2",  # Optimization terrain curriculum
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
@@ -187,17 +180,42 @@ gym.register(
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfgv2",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
-        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
+
+# DualGate-Attention-G1-v3
 gym.register(
     id="DualGate-Attention-G1-v3",  # Optimization terrain curriculum
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfgv3",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
+        "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+    },
+)
+gym.register(
+    id="DualGate-Attention-G1-Play-v3",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfgv3_PLAY",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
+        "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+        "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+        "gated_rsl_rl_cfg_entry_point": f"{agents.__name__}.gated_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+        "latent_rsl_rl_cfg_entry_point": f"{agents.__name__}.latent_asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
+    },
+)
+gym.register(
+    id="DualGate-Attention-G1-Eval-v3",  # Optimization terrain curriculum
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.attention_env_cfg:G1AttentionEnvCfgv3_EVAL",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.velocity_cnn_rsl_rl_ppo_cfg:G1RoughPPORunnerCfgWithSymmetryCfg",
         "mha_rsl_rl_cfg_entry_point": f"{agents.__name__}.mha_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
         "asymmetric_rsl_rl_cfg_entry_point": f"{agents.__name__}.asymmetric_rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
